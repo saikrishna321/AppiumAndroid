e477814e622e6b68c8561b25bcd9bb6a856d7cc1
saikrisv <saikrisv@INsaikrisv.local>
2015-09-14 11:21:01 +0530

    fixed issues

--- src/test/java/com/test/appium/dryrun/WordPressAndroidTest.java
+++ src/test/java/com/test/appium/dryrun/WordPressAndroidTest.java
@@ -28,7 +28,7 @@ public class WordPressAndroidTest extends BaseTest {
 		driver.findElement(By.id("com.android2.calculator3:id/digit5")).click();
 		driver.findElement(By.id("com.android2.calculator3:id/plus")).click();
 		driver.findElement(By.id("com.android2.calculator3:id/digit9")).click();
-		driver.findElement(By.id("com.android2.calculator3:id/equal ")).click();
+		driver.findElement(By.id("com.android2.calculator3:id/equal")).click();
 		waitForElement(By.xpath("com.android2.calculator3:id/cling_dismiss"), 30);
 		String num = driver.findElement(By.xpath("//android.widget.EditText[@index=0]")).getText();
 		System.out.println("Result : " + num);
